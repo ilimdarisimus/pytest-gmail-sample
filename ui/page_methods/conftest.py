250ffc635ed05dc0703405d7c2fecef5b6d0c64a
ilimdarisimus <54893877+ilimdarisimus@users.noreply.github.com>
2020-03-04 11:47:09 +0300

    Delete conftest.py

--- ui/page_methods/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-
-@pytest.fixture(scope="session")
-def browser():
-    driver = webdriver.Chrome(executable_path="./chromedriver")
-    yield driver
-    driver.quit()
